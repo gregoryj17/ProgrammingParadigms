9c6e208ed28f4ae507682a8e706d892dd5489ace
gregoryj17 <jaxtaxman@gmail.com>
2018-02-20 10:39:50 -0600

    Fixed build script.

--- Assignment 4/build.bash
+++ Assignment 4/build.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e -x
-javac Game.java View.java Controller.java Model.java Tube.java Json.java Mario.java
- Fireball.java Goomba.java Sprite.java
+javac Game.java View.java Controller.java Model.java Tube.java Json.java Mario.java Fireball.java Goomba.java Sprite.java
